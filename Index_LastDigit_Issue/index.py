f92373cb11b82a9a57b6f48367e1f3df19dd2090
Yohan Kulasinghe <yohankulasinghe@gmail.com>
2020-05-28 23:09:59 +0530

    Create index.py

---
+++ Index_LastDigit_Issue/index.py
@@ -0,0 +1,25 @@
+import numpy as np
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import math
+import xlrd
+
+loc = ("FIT16.xlsx")
+indexes = []
+names = []
+checkDigit = '3'
+
+
+wb = xlrd.open_workbook(loc)
+sheet = wb.sheet_by_index(0)
+sheet.cell_value(0, 0)
+
+for i in range(sheet.nrows):
+    if(sheet.cell_value(i, 1)[5:6] == checkDigit):
+        indexes.append(sheet.cell_value(i,1))
+        names.append(sheet.cell_value(i,2))
+    
+print("\n There are",len(names)," students having 3 as last digit of their index\n")
+for i, n in zip(indexes, names):
+    print("{} = {}".format(i,n))
